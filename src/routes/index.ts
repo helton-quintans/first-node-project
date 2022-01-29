028fc0da733c985efc154d4bfdb9bb5d7316c062
Helton Quintãns <helton.quit@gmail.com>
2022-01-28 23:38:40 -0300

    Usando o debbuger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,14 +2,15 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/users', (req, res) => {
-  const { name, email } = req.body;
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
 
   const user = {
     name,
     email,
   };
-  return res.json(user);
+
+  return response.json(user);
 });
 
 export default routes;
