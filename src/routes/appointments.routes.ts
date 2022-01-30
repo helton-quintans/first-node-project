2c5a9ea9321faba3f41cc2a0a25cf42c0ff13a1e
Helton Quintãns <helton.quit@gmail.com>
2022-01-30 16:19:55 -0300

    Create appointmentsRouter.get

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,16 +1,24 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointment';
+import { startOfHour, parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
 
   if (findAppointmentInSameDate) {
     return response
@@ -18,7 +26,7 @@ appointmentsRouter.post('/', (request, response) => {
     .json({ message: 'This appointment already booked' });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate)
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
