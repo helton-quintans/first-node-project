90d484e71dd773e8cd7da939dbfef63f2149a368
Helton Quintãns <helton.quit@gmail.com>
2022-02-04 23:43:32 -0300

    CreateAppointmentService

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,4 +1,31 @@
+import { response } from "express";
 import Appointment from "../models/Appointment";
-class CreateAppointmentService {
-  public execute( )
+import AppointmentsRepository from "../repositories/AppointmentsRepository";
+
+interface Request {
+  provider;
+  date: Date;
 }
+class CreateAppointmentService {
+  public execute({ provider, date }:Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = apointmentRepository.findByDate(
+      parsedDate,
+    );
+
+    if(findAppointmentInSameDate) {
+      return response
+        .status(400)
+        .json({message: 'This appointment is already booked'})
+    }
+    const appointment = appoitmentRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
